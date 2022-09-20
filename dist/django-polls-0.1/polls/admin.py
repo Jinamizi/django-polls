551f64897ae3bcc6f323d1265ec2d795ef450648
Jinamizi <tonnylango@gmail.com>
2022-09-20 23:49:24 +0300

    project sent to Github

---
+++ dist/django-polls-0.1/polls/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice, Hint
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline): #offers a tabular way of displaying inline related objects
+	model = Choice
+	extra = 3 #provide enough fields for 3 choices
+
+class HintInline(admin.StackedInline):
+	model = Hint
+
+class  QuestionAdmin(admin.ModelAdmin):
+	#fields = ['pub_date', 'question_text']
+	#spliting the form into fieldsets
+	fieldsets = [
+		(None, {'fields': ['question_text']}), 
+		('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+	]
+	inlines = [ChoiceInline, HintInline] #Choices objects are edited on the Question admin page
+
+	#To display individual fields
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	#add a "Filter" sidebar that lets people filter the change list by the pub_date field
+	list_filter = ['pub_date']
+	#add some search capabilities
+	search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice) 
